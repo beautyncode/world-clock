0dded8fe97692b09480b25cafc96d716d34358eb
Kenya M <beautyncode@users.noreply.github.com>
2022-12-29 01:06:22 -0600

    Added Modern JS for Time and Date

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,21 @@
+function updateTime() {
+  // Nairobi, Kenya
+  let nairobiElement = document.querySelector("#city-nairobi");
+  let nairobiDateElement = nairobiElement.querySelector(".date");
+  let nairobiTimeElement = nairobiElement.querySelector(".time");
+  let nairobiTime = moment().tz("Africa/Nairobi");
+  nairobiDateElement.innerHTML = nairobiTime.format("MMMM Do YYYY");
+  nairobiTimeElement.innerHTML = nairobiTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  // Lagos, Nigeria
+  let lagosElement = document.querySelector("#city-lagos");
+  let lagosDateElement = lagosElement.querySelector(".date");
+  let lagosTimeElement = lagosElement.querySelector(".time");
+  let lagosTime = moment().tz("Africa/Lagos");
+  lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+  lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
